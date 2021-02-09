af9d050a24531d153fbe4d5fc70850ef7108c0b7
“Daniel <59876783+dannychrist@users.noreply.github.com>
2021-02-09 12:25:13 -0500

    new changes february 9th

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// Components
+import Home from './pages/components/Home';
+import About from './pages/components/About';
+import SinglePost from './pages/components/SinglePost';
+import Post from './pages/components/Post';
+import Project from './pages/components/Project';
+
 const App = () => {
   return (
     <Router>
       <Switch>
-        
+        <Route component={Home} path='/' exact />
+        <Route component={About} path='/about' />
+        <Route component={SinglePost} path='/post/:slug' />
+        <Route component={Post} path='/post' />
+        <Route component={Project} path='/project' />
       </Switch>
     </Router>
   )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HttpsRedirect from 'react-https-redirect';
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <HttpsRedirect>
+    <App />
+  </HttpsRedirect>,
+  document.getElementById('root')
+);
